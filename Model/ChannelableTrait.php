7ab4fbf02c24ac01fd681e84d6b95cd6869b96f5
liverbool <nukboon@gmail.com>
2016-10-18 16:26:06 +0700

    decouple blamable from cms.

--- Model/ChannelableTrait.php
+++ Model/ChannelableTrait.php
@@ -2,65 +2,28 @@
 
 namespace Toro\Bundle\AdminBundle\Model;
 
-use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Channel\Model\ChannelInterface as BaseChannelInterface;
-use Doctrine\Common\Collections\ArrayCollection;
 
 trait ChannelableTrait
 {
     /**
-     * @var Collection|BaseChannelInterface[]
+     * @var BaseChannelInterface
      */
-    protected $channels;
+    protected $channel;
 
-    public function __construct()
+    /**
+     * {@inheritdoc}
+     */
+    public function getChannel()
     {
-        $this->channels = new ArrayCollection();
+        return $this->channel;
     }
 
     /**
-     * @return Collection|BaseChannelInterface[]
+     * {@inheritdoc}
      */
-    public function getChannels()
+    public function setChannel(BaseChannelInterface $channel = null)
     {
-        return $this->channels;
-    }
-
-    /**
-     * @param Collection $channels
-     */
-    public function setChannels(Collection $channels)
-    {
-        $this->channels = $channels;
-    }
-
-    /**
-     * @param BaseChannelInterface $channel
-     */
-    public function addChannel(BaseChannelInterface $channel)
-    {
-        if (!$this->hasChannel($channel)) {
-            $this->channels->add($channel);
-        }
-    }
-
-    /**
-     * @param BaseChannelInterface $channel
-     */
-    public function removeChannel(BaseChannelInterface $channel)
-    {
-        if ($this->hasChannel($channel)) {
-            $this->channels->removeElement($channel);
-        }
-    }
-
-    /**
-     * @param BaseChannelInterface $channel
-     *
-     * @return bool
-     */
-    public function hasChannel(BaseChannelInterface $channel)
-    {
-        return $this->channels->contains($channel);
+        $this->channel = $channel;
     }
 }
