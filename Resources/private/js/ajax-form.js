d2266ed1794bb250123a05d8042caedaf6d5ded9
liverbool <nukboon@gmail.com>
2017-04-09 21:01:29 +0700

    fix ajax-form

--- Resources/private/js/ajax-form.js
+++ Resources/private/js/ajax-form.js
@@ -29,7 +29,15 @@ $(document).on('click', '[data-ajax-form] button[type=submit],button[data-ajax-f
         processData: false, // Don't process the files
         contentType: false, // Set content type to false as jQuery will tell the server its a query string request
         success: function (res, textStatus, jqXHR) {
+            var callback = $form.data('ajax-callback');
+
+            if (callback) {
+                window[callback].call(res, $btn, $form, textStatus, jqXHR);
+                return;
+            }
+
             var $res = $(res), hasError = $res.find($form.data('error') || '.has-error').length;
+
             var reload = $form.data('reload');
 
             if (true === reload && !hasError) {
@@ -52,6 +60,8 @@ $(document).on('click', '[data-ajax-form] button[type=submit],button[data-ajax-f
                         if (typeof $target === 'string') {
                             $target = $($target);
                         }
+
+                        $('.modal').modal('hide');
 
                         $target.replaceWith($res);
                         $(document).trigger('dom-node-inserted', [$res]);
