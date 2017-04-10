068c8d1dd7d7e70a45383a939e2ebba681fe83e0
liverbool <nukboon@gmail.com>
2017-04-11 04:14:43 +0700

    add script

---
+++ Resources/private/js/notify-bell.js
@@ -0,0 +1,5 @@
+$(ducoment).on('click', '[data-notify-bell]', function() {
+    $(this).removeClass(function (index, className) {
+        return (className.match (/(^|\s)notify-bell--\S+/g) || []).join(' ');
+    });
+});
